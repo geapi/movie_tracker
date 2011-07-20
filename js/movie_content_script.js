b2d195145902e72e86880c65ed17492200c23c9c
Georg Apitz <georg@usebar.org>
2011-07-20 00:48:28 -0600

    movies with release date in the future are now parsed correctly and are available as ics download and Can be added to google and yahoo calendars

--- js/movie_content_script.js
+++ js/movie_content_script.js
@@ -8,21 +8,24 @@ if (window == top) {
 // Search the text nodes for a US-style mailing address.
 // Return null if none is found.
 var findMovie = function() {
-	console.log("trying to find a movie");
 	var show_times = $('div').find('h3.title span#view-showtimes').text().trim();
 	var copy_right = $('div').find('h3.title span.sosumi').text().trim();
 	var movie_title = $('div').find('h3.title').text().trim();
 	movie_title = movie_title.substring(show_times.length, (movie_title.length - copy_right.length)).trim();
-	console.log("movie title: "+ movie_title);
-	console.log("show times "+ show_times);
-    var release_date = show_times.split(":");
-    console.log(release_date[1].trim());
+//	console.log("show times "+ show_times);
+    var release_date_raw = show_times.split(":");
+    var release_date = release_date_raw[1].trim();
+//    console.log("release date:", release_date);
 	//var movie_mix = { title: movie_title,
 	//									showtimes: showtimes, 
 	//									copyright: copyright};
 	//return movie_mix[0];
-	return (movie_title + "<br/>" + show_times + "<br/>" + copy_right);
-	//return movie_title;
+    var movie_hash ={
+        title: movie_title,
+        release_date: release_date
+    }
+//	return (movie_title + "<br/>" + show_times + "<br/>" + copy_right);
+	return movie_hash;
 }
 
 
